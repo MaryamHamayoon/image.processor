8ae2d46dcd2c0f7ba813d8306bf36e145519d228
MaryamHamayoon <116061315+MaryamHamayoon@users.noreply.github.com>
2022-10-31 16:22:38 +0300

    Add files via upload

---
+++ week4-2.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+resim1= CV2. imread ("olives.jpg")
+cv2. imshow ("Brain" , resim1)
+#Resmin boyutunu ögrenmek icin:
+print (resiml.size)
+#Resmin veri tipini ögrenmek icin:
+print(resim1.dtype)
+#Resmin genisligini, yüksekligini ve kac kanaldan olustugunu ögrenmek için:
+print(resiml.shape)
+cv2.waitKey(0)
+cv2. destroyAllwindows()
