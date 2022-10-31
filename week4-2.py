0f314b796804624caa7313c663e01c193ab6ec63
MaryamHamayoon <116061315+MaryamHamayoon@users.noreply.github.com>
2022-10-31 16:24:24 +0300

    Add files via upload

---
+++ week4-2.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+resim1= CV2. imread ("olives.jpg")
+cv2. imshow ("Brain" , resim1)
+
+print (resiml.size)
+
+print(resim1.dtype)
+
+print(resiml.shape)
+cv2.waitKey(0)
+cv2. destroyAllwindows()
